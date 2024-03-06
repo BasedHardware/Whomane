cc97572df2d85968c4f2d531024e849821122fa4
Nik Shevchenko <43514161+kodjima33@users.noreply.github.com>
2024-03-06 00:56:31 -0800

    Update start.py

--- pi/start.py
+++ pi/start.py
@@ -2,35 +2,18 @@ import RPi.GPIO as GPIO
 import time
 import subprocess  # Import the subprocess module
 
-# Set the GPIO mode to BCM (Broadcom SOC channel numbering)
+
+
+
+
 GPIO.setmode(GPIO.BCM)
 
-# Set the pin number connected to the touch sensor
-TOUCH_PIN = 12
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# Set the GPIO pin as an input
-GPIO.setup(TOUCH_PIN, GPIO.IN)
+while True:
+    input_state = GPIO.input(15)
+    if input_state == False:
+        print('Button Pressed')
+        subprocess.run(["python","script.py"])  # Run script.py when the sensor is touched
 
-# Variable to track the touch sensor state
-prev_touch_state = GPIO.LOW  # Assuming the sensor is not touched initially
-
-try:
-    while True:
-        touch_state = GPIO.input(TOUCH_PIN)
-
-        if touch_state != prev_touch_state:
-            if touch_state == GPIO.HIGH:
-                # Sensor touched event
-                print("Touch sensor is touched!")
-                subprocess.run(["python","script.py"])  # Run script.py when the sensor is touched
-                
-            else:
-                # Sensor released event
-                print("Touch sensor is released!")
-
-        prev_touch_state = touch_state
-        time.sleep(0.1)  # A small delay to debounce the input
-        
-except KeyboardInterrupt:
-    # Clean up the GPIO settings on program exit
-    GPIO.cleanup()
+        time.sleep(0.2)
